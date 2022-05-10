34c16f1eb10d9a18131793ffff5342f7a04e739d
John Leung <john@johnleung.com>
2022-05-09 17:09:13 -0700

    [WIP] Simplify grammar

--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,12 @@ import ohm from 'ohm-js'
 
 const toAST = ohm.extras.toAST
 const parse = {}
-const parser = {
-  match: null // set in .parse()
-}
+const parser = {}
 export default parser
+
+const getEnclosedChilds = (node={}) => {
+  return node.child(1).child(0).children // child(1) left-terminal, eg. [, {, <, (
+}
 
 parse.literal = (literal={}) => {
   const { ctorName, sourceString } = literal.child(0)
@@ -21,12 +23,13 @@ parse.literal = (literal={}) => {
   }
   throw new Error(`literal of type: ${ctorName} requires handling`)
 }
-parse.Array = (iterNode={}) => {
+parse.List = (iterNode={}) => {
   return iterNode.ctorName === 'Commands'
-    ? parse.Commands(iterNode)
+    // ? parse.Commands(iterNode)
+    ? iterNode.child(0).children.map(c => parse.Command(c))
     : iterNode.children.map(c => parse.singleValue(c))
 }
-// TODO - merge w/ singleValue
+// TODO - merge w/ singleValue?
 parse.Value = (Value={}) => {
   const node = Value.child(0)
   switch (node.ctorName) {
@@ -35,10 +38,28 @@ parse.Value = (Value={}) => {
   }
   throw new Error(`Value of type: ${node.ctorName} requires handling`)
 }
+// parse.KeyValue = (node={}) => {
+//   const keyNode = node.child(0)
+//   const valNode = Value.child(0)
+
+//   let keyStr = keyNode.sourceString
+//   if (keyNode.ctorName === 'stringLiteral')
+//     keyStr = keyStr.slice(1, -1)
+
+
+//         const argChilds = Values.child(0).children
+//         let arg = {}
+//         for (let c of argChilds)
+//           arg = { ...arg, ...c.child(0).toAST(mapping)}
+//         if (!args.length)
+//           res.push(arg)
+//         break
+//       }
+
 parse.singleValue = (node={}) => {
   switch (node.ctorName) {
-    case '_terminal':
-      return node.sourceString
+    // case '_terminal':
+    //   return node.sourceString
     case 'literal':
       return parse.literal(node)
     case 'Value':
@@ -46,59 +67,45 @@ parse.singleValue = (node={}) => {
     case 'identifier':
       return String(node.sourceString)
     case 'List':
-    case 'Array':
-      return parse.Array(node.child(1))
+      return parse.List(node.child(1))
   }
   throw new Error(`Single value of ${node.ctorName} requires handling in parse.singleValue`)
 }
 
-parse.Commands = (CommandsNode={}) => {
-  const resultValue = []
-  for (let Command of CommandsNode.child(0).children) {
-    const [identifier, Values] = Command.children
-    const args = []
+parse.Command = (Command={}) => {
+  const [identifier, Values] = Command.children
+  const args = []
+  const res = [identifier.sourceString]
 
-    for (let valuesIter of Values.child(0).children) {
-      const Value = valuesIter.child(0)
-      switch (Value.ctorName) {
-        case 'List':
-        case 'Array': {
-          const argChilds = Value.child(1).child(0).children
-          args.push(argChilds.map(c => parse.singleValue(c)))
-          //* parse.singleValue will throw Unsupported error if nested list/array
-          break
+  for (let valuesIter of Values.child(0).children) {
+    const Value = valuesIter.child(0)
+    switch (Value.ctorName) {
+      case 'List':
+        res.push(getEnclosedChilds(Value)
+          .map(c => parse.singleValue(c)))
+        break
+      case 'KeyValuesMap': {
+        const argChilds = getEnclosedChilds(Value)
+        let arg = {}
+        for (let c of argChilds) {
+          const KeyValue = c.child(0)?.ctorName === 'KeyValue'
+            ? c.child(0) // single KeyValuesBraced
+            : c
+          if (KeyValue.child(0)?.ctorName !== '_terminal')
+            arg = { ...arg, ...KeyValue.toAST(mapping) }
         }
-        case 'KeyValuesBraced': {
-          const argChilds = Value.child(1).child(0).children
-          let arg = {}
-          for (let c of argChilds) {
-            const KeyValue = c.child(0)?.ctorName === 'KeyValue'
-              ? c.child(0) // single KeyValuesBraced
-              : c
-            if (KeyValue.child(0)?.ctorName !== '_terminal')
-              arg = { ...arg, ...KeyValue.toAST(mapping) }
-          }
-          args.push(arg)
-          break
-        }
-        case 'KeyValue': {
-          const argChilds = Values.child(0).children
-          let arg = {}
-          for (let c of argChilds)
-            arg = { ...arg, ...c.child(0).toAST(mapping)}
-          if (!args.length)
-            args.push(arg)
-          break
-        }
-        default: {
-          args.push(parse.singleValue(Value))
-          break
-        }
+        res.push(arg)
+        break
+      }
+
+      default: {
+        res.push(parse.singleValue(Value))
+        break
       }
     }
-    resultValue.push([identifier.sourceString, ...args])
   }
-  return resultValue
+  console.log(res)
+  return res
 }
 
 /**
@@ -123,8 +130,7 @@ const mapping = {
       case 'KeyValuesBraced':
         return { [keyStr]: parse.Value(valNode.child(1)) }
       case 'List':
-      case 'Array':
-        return { [keyStr]: parse.Array(valNode.child(1)) }
+        return { [keyStr]: parse.List(valNode.child(1)) }
       default:
         throw new Error(`case ${val.ctorName} requires handling`)
     }
@@ -151,7 +157,6 @@ const mapping = {
  * } options 
  */
 parser.parse = (userText, g, options={}) => {
-  parser.match = g.match
   const match = g.match(userText)
   const { outputAST, outputMD } = options
 
--- test/commands.test.js
+++ test/commands.test.js
@@ -3,63 +3,67 @@ import { parse } from '../src/api'
 
 // TODO: (( )) & [[ ]]
 test(`Commands - Single argument`, () => {
-  expect(parse(`do: ( // comment after '('
+    // move [[1 2]];
+  expect(parse(`do: [ // comment after '['
     move 1 2;
     move [1 2];
-  )`).result)
+  ]`).result)
   .toStrictEqual({ do: [
     ['move', 1, 2],
     ['move', [1, 2]],
+    // ['move', [[1, 2]]],
   ]})
 })
 
-test(`Commands - Random commas in a 1-liner`, () => {
-  expect(parse(`do: ( move 1 2,  3 -4; )`).result)
+test(`Commands - 1-liner w/ random comma between args`, () => {
+  expect(parse(`do: [ move 1 2,  3 -4; ]`).result)
   .toStrictEqual({ do: [
     ['move', 1, 2, 3, -4],
   ]})
 })
 
-test(`Commands - Pairs of array/lists`, () => {
-  expect(parse(`do: (
+test(`Commands - Variations of array/list args`, () => {
+  expect(parse(`do: [
     move [1,2] [3 -4];
-    move [1 2] [3 -4];
-    move (1 2) (3,-4);
-    move (1 2) (3 -4); // comment last line
-  )`).result)
+    move [1 2],[3 -4];
+    move [1 2] [3 -4]; // comment last line
+  ]`).result)
   .toStrictEqual({ do: [
-    ['move', [1, 2], [3, -4]],
     ['move', [1, 2], [3, -4]],
     ['move', [1, 2], [3, -4]],
     ['move', [1, 2], [3, -4]],
   ]})
 })
 
-test(`Commands - Single keyvalues map`, () => {
-  expect(parse(`do: (
+test(`Commands - Variations of Single keyvalues map arg`, () => {
+  expect(parse(`do: [
     turn { x:1, y:2 };
-    turn x:1, y:2;
-    // comment before ')'
-  )`).result)
+    turn { x:1  y:2 };
+    // comment before ']'
+  ]`).result)
   .toStrictEqual({ do: [
     ['turn', {x: 1, y: 2}],
     ['turn', {x: 1, y: 2}],
   ]})
 })
 
-test(`Commands - Multiple keyvalue maps`, () => {
-  expect(parse(`do: (
-    turn { x:1},{y:2 };
-  ) // comment after')'`).result)
+test.skip(`Commands - Variations of Multiple keyvalues map args`, () => {
+    // turn x:1  y:2;
+    // turn {x:1} {y:2};
+  expect(parse(`do: [
+    turn x:1, y:2;
+  ] // comment after')'`).result)
   .toStrictEqual({ do: [
+    // ['turn', {x: 1}, {y: 2}],
+    // ['turn', {x: 1}, {y: 2}],
     ['turn', {x: 1}, {y: 2}],
   ]})
 })
 
-test(`Commands - Mixed strings and value args`, () => {
-  expect(parse(`do: (
+test(`Commands - Mixed strings and values args`, () => {
+  expect(parse(`do: [
     turn left 1 right 2;
-  )`).result)
+  ]`).result)
   .toStrictEqual({ do: [
     ['turn', 'left', 1, 'right', 2],
   ]})
